ed0935e24ce7788cc402ef65b241f6301900adac
Meelod Waheed <meelod.waheed360@gmail.com>
2023-12-06 22:29:53 -0800

    update

--- random_forest_synthetic.py
+++ random_forest_synthetic.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from imblearn.over_sampling import SMOTE
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load your dataset
 df = pd.read_csv('/Users/meeps360/cs158-final-project/week_approach_maskedID_timeseries.csv')
@@ -25,7 +27,7 @@ X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, te
 clf = RandomForestClassifier(n_estimators=100, random_state=42)
 clf.fit(X_train, y_train)
 
-#testing oringal dataset
+#testing original dataset
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # Make predictions
@@ -33,3 +35,30 @@ y_pred = clf.predict(X_test)
 
 # Evaluate the model
 print(classification_report(y_test, y_pred))
+
+#create a table showing trends in what makes a player more likely to be injured
+#showing the top 10 features with the highest feature importance and label columns
+feature_imp = pd.Series(clf.feature_importances_, index=X_train.columns).sort_values(ascending=False)
+
+#label the columns
+feature_imp.columns = ['Feature', 'Importance']
+print(feature_imp.head(10))
+#what was the least important feature?
+print(feature_imp.tail(1))
+
+#export the table to a csv
+feature_imp.to_csv('feature_imp.csv')
+
+# #create a graph showing the top 10 features with the highest feature importance
+# # Creating a bar plot
+# sns.barplot(x=feature_imp[:10], y=feature_imp[:10].index)
+# # Add labels to your graph
+# plt.xlabel('Feature Importance Score')
+# plt.ylabel('Features')
+# plt.title("Visualizing Important Features")
+# plt.legend()
+# plt.show()
+# plt.savefig('feature_imp.png')
+
+#what are all the features?
+print(feature_imp.index)
